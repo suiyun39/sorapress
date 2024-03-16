57f06b868fe5643cafb909fd0726e3d816baa3d9
suiyun39 <suiyun39@gmail.com>
2024-03-16 23:12:34 +0800

    feat: 完成入口模板

--- src/plugins/sorapress/index.ts
+++ src/plugins/sorapress/index.ts
@@ -1,7 +1,15 @@
 import type { Plugin } from 'vite'
+import mustache from 'mustache'
+import entryTemplate from './template/entry.mustache?raw'
 
 function cleanUrl(url: string) {
   return url.replace(/#.*$/su, '').replace(/\?.*$/su, '')
+}
+
+function renderEntry() {
+  // todo: 从配置文件中获取信息
+  const config = { title: 'SoraPress' }
+  return mustache.render(entryTemplate, config)
 }
 
 export function sorapressPlugin(): Plugin {
@@ -11,16 +19,19 @@ export function sorapressPlugin(): Plugin {
     configureServer(server) {
       // 在此处提供入口文件, 并将 client 注入到其中
       return () => {
-        server.middlewares.use((req, res, next) => {
+        server.middlewares.use(async (req, res, next) => {
           const url = req.url && cleanUrl(req.url)
 
           if (!url || !url.endsWith('.html')) {
             return void next()
           }
 
+          const entry = renderEntry()
+          const html = await server.transformIndexHtml(url, entry, req.originalUrl)
+
           res.statusCode = 200
           res.setHeader('Content-Type', 'text/html')
-          res.end('<h1>Hello Sora!</h1>')
+          res.end(html)
         })
       }
     },
