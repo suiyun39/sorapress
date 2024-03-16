00687c50ecf00efc47a35890f1eb768cd59110db
suiyun39 <suiyun39@gmail.com>
2024-03-16 23:18:19 +0800

    feat: 添加 solid-js 编译支持

--- src/server.ts
+++ src/server.ts
@@ -1,8 +1,9 @@
 import { createServer as createViteServer } from 'vite'
+import solidPlugin from 'vite-plugin-solid'
 import { sorapressPlugin } from './plugins/sorapress'
 
 export async function createServer() {
   return createViteServer({
-    plugins: [sorapressPlugin()],
+    plugins: [sorapressPlugin(), solidPlugin()],
   })
 }
